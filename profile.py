52dc414b50d08f3654056ae64102e2b17f0092f5
Kiahj03 <90876523+Kiahj03@users.noreply.github.com>
2023-01-31 08:34:36 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,9 +1,10 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
-#create a reques object to start building the RSpec
+# Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
-#create a XenVM
+# Create a XenVM
+node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
 node.routable_control_ip = "true"
 
@@ -11,5 +12,5 @@ node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
 node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
 
-#print the RSpec to the enclosing page
+# Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
